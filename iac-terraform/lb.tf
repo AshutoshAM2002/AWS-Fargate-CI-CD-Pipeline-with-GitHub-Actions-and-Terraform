eee9d230fc0483b7fc55682bc3239a53e1e85d22
Ashutosh Mahajan <ashutoshmahajan2002@gmail.com>
2023-03-24 12:54:29 +0530

    added terraform code

---
+++ iac-terraform/lb.tf
@@ -0,0 +1,24 @@
+resource "aws_lb" "lb" {
+  name            = "express-lb"
+  subnets         = aws_subnet.public.*.id
+  security_groups = [aws_security_group.lb.id]
+}
+
+resource "aws_lb_target_group" "target_group" {
+  name        = "express-target-group"
+  port        = 80
+  protocol    = "HTTP"
+  vpc_id      = aws_vpc.default.id
+  target_type = "ip"
+}
+
+resource "aws_lb_listener" "listener" {
+  load_balancer_arn = aws_lb.lb.id
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    target_group_arn = aws_lb_target_group.target_group.id
+    type             = "forward"
+  }
+}
